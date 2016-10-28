7efef7bf1e578c51ac5d976ae6d9b1ce4e5b29c4
Sten-Oliver Salumaa <salumaa@ut.ee>
2016-10-28 18:27:30 +0300

    käisin praksis ja näppisin noxi ja kirjutasin kaks näidet juurde. Also, muutsin 'homo.bpmn' ära 'test.bpmn'

--- src/ut/systems/modelling/ConverterPlugin.java
+++ src/ut/systems/modelling/ConverterPlugin.java
@@ -6,7 +6,12 @@ import org.processmining.framework.plugin.annotations.Plugin;
 import org.processmining.framework.plugin.annotations.PluginVariant;
 
 import org.processmining.models.graphbased.directed.bpmn.BPMNDiagram;
+import org.processmining.models.graphbased.directed.bpmn.BPMNNode;
 import org.processmining.models.graphbased.directed.petrinet.Petrinet;
+import org.processmining.models.graphbased.directed.petrinet.elements.Transition;
+import org.processmining.models.graphbased.directed.petrinet.impl.PetrinetImpl;
+
+import java.util.Set;
 
 
 @Plugin(
@@ -28,8 +33,37 @@ public class ConverterPlugin {
     public static Petrinet optimizeDiagram(UIPluginContext context, BPMNDiagram diagram) {
         Petrinet pn = null;
 
+
+        //MyBPMNModel myBPMNModel = Parser.getMyBPMNModel(diagram);
+        //MyPetriNet myPetriNet = MyConverter.getPN(myBPMNModel);
+
+        //lets say i have a class called parser
+        //with method getmybpmnmodeol
+        //lets say we have class myconverter with method getPN
+
+        //sth from practice session question
+        Transition t  = new Transition("", pn);
+        t.setInvisible(true);
+
+
+        //Set<BPMNNode> set = diagram.getNodes();
+
+
+
+//        for (BPMNNode s : set) {
+//            System.out.println(s.toString());
+//        }
+
+
+
+
+
+
 //        MyBPMNModel myBPMNModel = getMyBPMNModel(diagram);
-//        pn = MyConverter.getPN(myBPMNModel);
+//        pn = MyConverter.getPN(myBPMNModel); // fill this object with data from myPNObject
+
+        pn = new PetrinetImpl("myPetriNet");
+        pn.addPlace("Start_place");
 
         return pn;
     }
